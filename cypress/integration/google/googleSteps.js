c21d13a012ecb6705838cf3fec1f6da792fe2938
pavi-github <pavishanmugam17@gmail.com>
2021-03-02 21:37:46 +0000

    locator update in stepdef

--- cypress/integration/google/googleSteps.js
+++ cypress/integration/google/googleSteps.js
@@ -3,8 +3,7 @@ import { Given , And , Then , When} from "cypress-cucumber-preprocessor/steps";
 const url = 'https://www.google.co.uk'
 
 Given('As someone interested in cryptocurrencies', () => {
-  cy.visit(url)
-  cy.reload()
+  cy.visit(url).reload()
 })
 
 When('I type {string} in Google search input', (searchtxt) => {
@@ -12,7 +11,7 @@ When('I type {string} in Google search input', (searchtxt) => {
 })
 
 And('I click on the search button', () => {    
-    cy.get("input[id='gbqfbb']").prev().click()     
+    cy.get("#gbqfbb").prev().click()     
 })
 
 And('I press Enter', () => {    
@@ -20,7 +19,7 @@ And('I press Enter', () => {
 })
 
 Then('I should see {string} as one of the search results', (verifylink) => {         
-    cy.get("div[id='search']").contains(verifylink)
+    cy.get("#search").contains(verifylink)
 })
 
 
